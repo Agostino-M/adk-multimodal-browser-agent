046721746e4a5e1f9ff0982ba56184f6f16a15b7
Agostino Messina <agomessina@gmail.com>
2026-04-11 00:24:28 +0200

    add execution and verification agents

---
+++ browser_agent/subagents/execution_agent.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+
+from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+
+from browser_agent.prompt import web_execution_prompt
+from browser_agent.callbacks import inject_current_task, validate_execution_tools
+from browser_agent.browser import BrowserManager
+
+load_dotenv()
+
+ENG_API_KEY = os.getenv("ENG_API_KEY")
+if not ENG_API_KEY:
+    raise ValueError("ENG_API_KEY not found.")
+
+browser = BrowserManager(show_browser=True)
+browser_tools = [
+    browser.click,
+    browser.type,
+    browser.scroll,
+    browser.goto_url,
+    browser.get_state,
+    browser.press_key,
+    browser.wait,
+    browser.close,
+]
+
+execution_agent = LlmAgent(
+    name="execution_agent",
+    model=LiteLlm(
+        api_base="http://172.30.22.153/v1",
+        api_key=ENG_API_KEY,
+        model="openai/Qwen3_5_9B",
+        chat_template_kwargs={"enable_thinking": False},
+    ),
+    instruction=web_execution_prompt,
+    output_key="execution_output",
+    tools=browser_tools,
+    include_contents="none",
+    before_model_callback=inject_current_task,
+    before_tool_callback=validate_execution_tools,
+)
---
+++ browser_agent/subagents/verification_agent.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+
+from browser_agent.prompt import verification_prompt
+from ..callbacks import inject_current_task
+from browser_agent.state import update_current_subtask
+
+
+load_dotenv()
+
+ENG_API_KEY = os.getenv("ENG_API_KEY")
+if not ENG_API_KEY:
+    raise ValueError("ENG_API_KEY not found.")
+
+
+verification_agent = LlmAgent(
+    name="verification_agent",
+    model=LiteLlm(
+        api_base="http://172.30.22.153/v1",
+        api_key=ENG_API_KEY,
+        model="openai/Qwen3_5_9B",
+        chat_template_kwargs={"enable_thinking": False},
+    ),
+    instruction=verification_prompt,
+    tools=[update_current_subtask],
+    include_contents="none",
+    before_model_callback=inject_current_task,
+)
